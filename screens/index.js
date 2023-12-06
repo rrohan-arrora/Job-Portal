12861ed729666c9090df351aec3a4319a69c742d
Aaksh Gadekar <an14092000@gmail.com>
2023-12-06 16:28:06 +0530

    Added common and various home components Added respective styling as well

---
+++ screens/index.js
@@ -0,0 +1,13 @@
+//Screen header button
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+
+// home screen
+import NearbyJobs from "./home/nearby/Nearbyjobs";
+import Popularjobs from "./home/popular/Popularjobs";
+import Welcome from "./home/welcome/Welcome";
+
+export {
+    NearbyJobs,
+    Popularjobs, ScreenHeaderBtn,
+    Welcome
+};
