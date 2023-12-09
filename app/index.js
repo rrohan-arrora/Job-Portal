ba0e26734ed26bdf37ce3fb7499882098b1674a4
Aaksh Gadekar <an14092000@gmail.com>
2023-12-09 23:36:46 +0530

    Added code to show screen header  button Added code to Home screen layout

--- app/index.js
+++ app/index.js
@@ -5,10 +5,38 @@ import {COLORS, icons, images, SIZES} from '../constants';
 import { Popularjobs, NearbyJobs, Welcome, ScreenHeaderBtn } from "../screens";
 
 const Home = () => {
+    const router = useRouter();
+    
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView  style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}>
+                    <View
+                        style={{
+                            flex: 1,
+                            padding: SIZES.medium
+                        }}
+                    >
+                        <Welcome />
+                        <Popularjobs />
+                        <NearbyJobs />
+                    </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
